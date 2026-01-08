6bbb3204e2e8f9534d8add944eb68c396f4df960
Basil <kidnamedheisenberg@gmail.com>
2026-01-08 23:54:58 +0800

    Update omori.sh

--- omori.sh
+++ omori.sh
@@ -6,24 +6,25 @@ echo "|     - OMORI PATCH TOOL FOR INTEL -    |"
 echo "\\---------------------------------------/"
 echo ""
 
-OMORI=~/Library/Application\ Support/Steam/steamapps/common/OMORI
+OMORI=~/Library/Application\ Support/Steam/steamapps/common/OMORI # Directory of Omori
 
-if [ ! -d "${OMORI}" ]; then
+if [ ! -d "${OMORI}" ]; then # Looks for Omori
   echo "[!!] Please make sure you have Omori installed on steam before using this tool.";
   exit 1;
 fi;
 
-echo "Backing up original Omori.app.."
+echo "Backing up original OMORI copy.."
 if [ -f "${OMORI}.original" ]; then
-  rm -rf "${OMORI}.original"
+  rm -rf "${OMORI}.original" # Removes original backup
 fi;
-cp -r "${OMORI}" "${OMORI}.original";
+cp -r "${OMORI}" "${OMORI}.original"; # Copies files to backup folder
 
 TMPFOLDER=$(mktemp -d /tmp/omori-patch.XXXXXX) || exit 1
-cd "$TMPFOLDER";
+cd "$TMPFOLDER"; # Creates temporary folder
 
-mv "${OMORI}" "./Omori.original.app";
+mv "${OMORI}" "./OMORI.original"; # Moves game files to temporary folder
 
+# Downloads required files
 echo "Downloading nwjs for Intel.."
 curl -#L -o nwjs.zip https://dl.node-webkit.org/v0.106.1/nwjs-v0.106.1-osx-x64.zip
 echo "Downloading node polyfill patch.."
@@ -34,27 +35,29 @@ curl -#L -o greenworks-osxx64.node https://github.com/BasilGunderson/omori-apple
 echo "Downloading steamworks api.."
 curl -#L -o steam.zip https://github.com/BasilGunderson/omori-apple-silicon-and-intel/releases/download/v1.0.0/steam.zip
 
+# Extracts specific archives
 echo "Extracting nwjs.."
 unzip -q nwjs.zip
 echo "Extracting steamworks.."
 unzip -qq steam.zip
 
+# Patches game by moving files
 echo "Patching game.."
-mv "./nwjs-v0.106.1-osx-x64/nwjs.app" "./Omori.app"
-mv -f ./Omori.original.app/Contents/Resources/app.nw ./Omori.app/Contents/Resources/
-mv -f ./Omori.original.app/Contents/Resources/app.icns ./Omori.app/Contents/Resources/
-mv -f ./node-polyfill-patch.js ./Omori.app/Contents/Resources/app.nw/js/libs/
-mv -f ./greenworks.js ./Omori.app/Contents/Resources/app.nw/js/libs/
-mv -f ./greenworks-osxx64.node ./Omori.app/Contents/Resources/app.nw/js/libs/
-mv -f ./steam/libsteam_api.dylib ./Omori.app/Contents/Resources/app.nw/js/libs/
-mv -f ./steam/libsdkencryptedappticket.dylib ./Omori.app/Contents/Resources/app.nw/js/libs/
+mv "./nwjs-v0.106.1-osx-x64/nwjs.app" "./OMORI"
+mv -f ./Omori.original/Contents/Resources/app.nw ./OMORI/Contents/Resources/
+mv -f ./Omori.original/Contents/Resources/app.icns ./OMORI/Contents/Resources/
+mv -f ./node-polyfill-patch.js ./OMORI/Contents/Resources/app.nw/js/libs/
+mv -f ./greenworks.js ./OMORI/Contents/Resources/app.nw/js/libs/
+mv -f ./greenworks-osxx64.node ./OMORI/Contents/Resources/app.nw/js/libs/
+mv -f ./steam/libsteam_api.dylib ./OMORI/Contents/Resources/app.nw/js/libs/
+mv -f ./steam/libsdkencryptedappticket.dylib ./OMORI/Contents/Resources/app.nw/js/libs/
 
-echo "Finished. Moving patched Omori.app back to original location.."
-mv "./Omori.app" "${OMORI}"
+echo "Finished. Moving patched game back to original location.."
+mv "./OMORI" "${OMORI}"
 
 echo ""
-echo "Done! You can now launch Omori."
-echo "Note that if you update Omori or check the integrity of the game files, you'll need to reapply the patch."
+echo "Done! Launch OMORI through Steam."
+echo "Note that if you update OMORI or check the integrity of the game files, you'll need to reapply the patch."
 echo ""
 echo ""
 
